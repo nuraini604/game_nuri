71a57c585db672df6da364630380044e1b47ebbf
nuraini604 <nuraiini604@gmail.com>
2025-06-21 10:06:00 +0700

    Project Akhir

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private CharacterController controller;
+    private Animator animator;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Cek apakah menyentuh tanah
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            animator.SetBool("IsJumping", false);
+        }
+
+        // Ambil input dari keyboard (tanpa smoothing)
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        float moveAmount = new Vector2(x, z).magnitude;
+
+        // Kirim ke Animator
+        animator.SetFloat("Speed", moveAmount);
+
+        // Gerakan horizontal
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Lompat
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("IsJumping", true);
+        }
+
+        // Tambahkan gravitasi
+        velocity.y += gravity * Time.deltaTime;
+
+        // Gerakan vertikal (gravity / lompat)
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    // DEBUG: Visualisasi groundCheck sphere di Scene View
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+        }
+    }
+}
